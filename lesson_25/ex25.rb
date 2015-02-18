53169cdb90111d4a2887adb0e45c50505c4c4035
Anthony Falzetti <Anthony.falzetti@gmail.com>
2015-02-18 10:40:32 -0700

    adding lesson_25 to repo.

---
+++ lesson_25/ex25.rb
@@ -0,0 +1,58 @@
+module Ex25
+  # This function will break up words for us.
+  def Ex25.break_words(stuff)
+    # Split turns and array into a string
+    # in this case we are splitting on the space.
+    words = stuff.split(' ')
+    return words
+  end
+  
+  # Sorts the words.
+  def Ex25.sort_words(words)
+    # sort -> sorts in alphabetical order.
+    return words.sort
+  end
+  
+  # Prints the first word after shifting it off.
+  def Ex25.print_first_word(words)
+    # shift returns the first element in the array
+    word = words.shift
+    puts word
+  end
+  
+  # Prints the last word after popping it off.
+  def Ex25.print_last_word(words)
+    # pop removes the last element in the array
+    word = words.pop
+    puts word
+  end
+  
+  # Takes in a full sentence and returns the sorted words.
+  def Ex25.sort_sentence(sentence)
+    # break_words splits the string into an array
+    words = Ex25.break_words(sentence)
+    # sort_words takes the array and sorts
+    return Ex25.sort_words(words)
+  end
+  
+  # Prints the first and last words of the sentence.
+  def Ex25.print_first_and_last(sentence)
+    # break_words splits the string into an array
+    words = Ex25.break_words(sentence)
+    # print_first_word returns the first element in the array
+    Ex25.print_first_word(words)
+    # print_last_word removes the last element in the array
+    Ex25.print_last_word(words)
+  end
+  
+  # Sorts the words then prints the first and last one.
+  def Ex25.print_first_and_last_sorted(sentence)
+    # Takes a full sentence and splits it into an array and then sorts it
+    words = Ex25.sort_sentence(sentence)
+    # print_first_word returns the first element in the array
+    Ex25.print_first_word(words)
+    # print_last_word removes the last element in the array
+    Ex25.print_last_word(words)
+  end
+  
+end
