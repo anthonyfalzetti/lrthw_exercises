50f654a5ec0deff1c33c2a6b468e1bc968f84dcb
Anthony Falzetti <Anthony.falzetti@gmail.com>
2015-03-04 17:53:57 -0700

    The beginning of many updates so Rubocop stops hating me

--- lesson35/ex35.rb
+++ lesson35/ex35.rb
@@ -1,16 +1,16 @@
 def gold_room
   puts 'This room is full of gold. How much do you take?'
-  
+
   print '> '
   choice = $stdin.gets.chomp
-  
+
   # this line has a bug, so fix it.
   if choice.to_i > 0
     how_much = choice.to_i
   else
     dead('Man, learn to type a number.')
   end
-  
+
   if how_much < 50
     puts "Nice, you're not greedy, you win!"
     exit(0)
@@ -25,11 +25,11 @@ def bear_room
   puts 'The fat bear is in front of another door'
   puts 'How are you going to move the bear?'
   bear_moved = false
-  
+
   while true
     print '> '
     choice = $stdin.gets.chomp
-    
+
     if choice == 'take honey'
       dead('The bear looks at you then slaps your face off.')
     elsif choice == 'taunt bear' && !bear_moved
@@ -39,7 +39,7 @@ def bear_room
       dead('The bear gets pissed off and chews your leg off.')
     elsif choice == 'open door' && bear_moved
       gold_room
-    else 
+    else
       puts 'I got no idea what that means.'
     end
   end
@@ -49,15 +49,15 @@ def cthulhu_room
   puts 'Here you see the great evil Cthulhu'
   puts 'He, it, whatever stares at you and you go insane.'
   puts 'Do you flee for your life or eat your head?'
-  
+
   print '> '
   choice = $stdin.gets.chomp
-  
+
   if choice.include? 'flee'
     start
   elsif choice.include? 'head'
     dead('Well that was tasty!')
-  else 
+  else
     cthulhu_room
   end
 end
@@ -71,10 +71,10 @@ def start
   puts 'You are in a dark room.'
   puts 'There is a door to your right and left.'
   puts 'Which one do you take?'
-  
+
   print '> '
   choice = $stdin.gets.chomp
-  
+
   if choice == 'left'
     bear_room
   elsif choice == 'right'
